db6dcb83ae888d4956679630e5386e25b2d5e417
Vitalij Schwab <schwab.vitalij@gmail.com>
2023-11-28 17:20:29 +0100

    Code verbessert

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,68 +1,46 @@
 import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from "./Header";
-import CreateCustomer from "./CreateCustomer";
-import ViewCustomerData from "./ViewCustomerData";
+import "../styles/styles.css";
+import Header from "./header/Header";
+import CreateCustomer from "./createCustomer/CreateCustomer";
+import ViewCustomer from "./viewCustomer/ViewCustomer";
 import axios from "axios";
+
+const API_URL = "http://localhost:3001/customers";
 
 function App() {
     const [customerData, setCustomerData] = useState([]);
     const [showCustomerData, setShowCustomerData] = useState(false);
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchCustomerData = async () => {
             try {
-                const response = await axios.get("http://localhost:3001/customers");
+                const response = await axios.get(API_URL);
                 setCustomerData(response.data);
             } catch (error) {
                 console.error("Error fetching customer data:", error);
             }
         };
 
-        fetchData();
+        fetchCustomerData();
     }, [customerData]);
 
     return (
         <>
             <Header
-                onFetchCustomerData={() => setShowCustomerData(true)}
-                onGetCreateCustomer={() => setShowCustomerData(false)}
+                onSetShowCustomerDataTrue={() => setShowCustomerData(true)}
+                onSetShowCustomerDataFalse={() => setShowCustomerData(false)}
             />
-            {!showCustomerData ? (
-                <CreateCustomer
+            {showCustomerData ? (
+                <ViewCustomer
                     customerData={customerData}
                     setCustomerData={setCustomerData}
                 />
             ) : (
-                <div className="container">
-                    <h2 className="my-4 text-center text-muted">Kundendaten</h2>
-                    <div className="table-responsive small">
-                        <table className="table table-striped table-sm">
-                            <thead className="text-center">
-                                <tr>
-                                    <th scope="col">Vorname</th>
-                                    <th scope="col">Nachname</th>
-                                    <th scope="col">E-Mail</th>
-                                    <th scope="col">Telefonnummer</th>
-                                    <th scope="col">Einstellungen</th>
-                                </tr>
-                            </thead>
-                            <tbody className="text-center">
-                                {customerData.map(customer => (
-                                    <ViewCustomerData
-                                        key={customer.id}
-                                        id={customer.id}
-                                        firstName={customer.first_name}
-                                        lastName={customer.last_name}
-                                        email={customer.email}
-                                        phoneNumber={customer.phone_number}
-                                        setCustomerData={setCustomerData}
-                                    />
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                <CreateCustomer
+                    customerData={customerData}
+                    setCustomerData={setCustomerData}
+                />
             )}
         </>
     )
