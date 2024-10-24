80c067dce9a120682ba297aded354e1711537a6d
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-10-24 11:35:52 +0200

    change env

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/vitalijschwab/Desktop/projects/crm/backend/env"
+set -gx VIRTUAL_ENV "/Users/vitalijschwab/Desktop/projects/crm/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
